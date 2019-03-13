e0ee7029781c1eae2a8648964bcf085529b9bfd4
Kevin Yeh <revinyeh@gmail.com>
2019-03-13 15:16:29 -0700

    delete default react files , and hooked with basic react readux reducer with all dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,11 @@ module.exports = {
         "react"
     ],
     "rules": {
-    }
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+    },
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+    ]
 };
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+  input: 'user input temp'
+})
